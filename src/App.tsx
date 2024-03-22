d03bfb015fdb31796512cd538da8b189cf00417e
Kim-minjung99 <minjung5229@naver.com>
2024-03-22 17:09:06 +0900

    Fix : test commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Suspense } from 'react';
 import ErrorBoundaryCustom from './components/ErrorBoundary/ErrorBoundaryCustom';
 import {ErrorBoundary} from 'react-error-boundary';
 
+
 // TODO : 스텝 순서대로 따라가는 로직이라고 생각하고 하나의 흐름을 퍼럼이라고 생각하고
 // 스텝의 흐름을 훅으로 직접 만들어보자.
 // AsyncComponent를 이용하여 굳이 에러시에 발생하는 컴포넌트에 대해 신경쓸 필요 없다. 커스텀으로 만들어두면 되기 때문이다.
